344a19a43c31d103334c8b54ff889a4f824dc026
toribird-2048 <fly-toribird@outlook.jp>
2024-10-04 00:20:50 +0900

    Genarating Images

---
+++ main.py
@@ -0,0 +1,54 @@
+import numpy as np
+import time
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+height, width = 10,10
+blocks = np.zeros((height, width))
+delta_blocks = np.zeros((height, width))
+spread_rate = 0.1
+
+fig = plt.figure()
+images = []
+
+"""
+for k in range(height):
+    for l in range(width):
+        blocks[k,l] = np.random.randint(0, 100)
+"""
+
+blocks[0,0] = 100
+
+for k in range(100):
+    print(blocks)
+    for i in range(height):
+        for j in range(width):
+            if i == 0:
+                delta_blocks[i,j] += 0
+            else :
+                delta_blocks[i,j] += (blocks[i-1,j] - blocks[i,j]) * spread_rate
+            
+            if j == 0:
+                delta_blocks[i,j] += 0
+            else :
+                delta_blocks[i,j] += (blocks[i,j-1] - blocks[i,j]) * spread_rate
+            
+            if i == height-1:
+                delta_blocks[i,j] += 0
+            else :
+                delta_blocks[i,j] += (blocks[i+1,j] - blocks[i,j]) * spread_rate
+            
+            if j == width-1:
+                delta_blocks[i,j] += 0
+            else :
+                delta_blocks[i,j] += (blocks[i,j+1] - blocks[i,j]) * spread_rate
+            
+    blocks += delta_blocks
+    delta_blocks = np.zeros((height, width))
+    im = plt.imshow(blocks)
+    cb = plt.colorbar()
+    plt.savefig(f"images//{k}.png")
+    cb.remove()
+    plt.cla()
+    #time.sleep(1)

6735d8b6fc3072c1eb1dfe683d96d5f8617edc0a
toribird-2048 <fly-toribird@outlook.jp>
2024-10-05 14:14:10 +0900

    spread_rates

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import numpy as np
 import time
 from PIL import Image
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import os
 
 height, width = 10,10
 blocks = np.zeros((height, width))
 delta_blocks = np.zeros((height, width))
-spread_rate = 0.1
-max = 100
+spread_rates = np.full((height, width),0.1)
+max = 200
 min = 0
-cycle = 100
+cycle = 3000
 
 fig = plt.figure()
 images = []
@@ -22,43 +22,50 @@ for k in range(height):
 """
 
 blocks[0,0] = 100
+spread_rates[5,5] = 0.01
+spread_rates[6,5] = 0.05
+spread_rates[5,6] = 0.05
+spread_rates[6,6] = 0.05
 
 for k in range(cycle):
     print(blocks)
+    blocks[0,0] = 100
     for i in range(height):
         for j in range(width):
             if i == 0:
                 delta_blocks[i,j] += 0
             else :
-                delta_blocks[i,j] += (blocks[i-1,j] - blocks[i,j]) * spread_rate
+                delta_blocks[i,j] += (blocks[i-1,j] - blocks[i,j]) * spread_rates[i,j] * spread_rates[i-1,j]
             
             if j == 0:
                 delta_blocks[i,j] += 0
             else :
-                delta_blocks[i,j] += (blocks[i,j-1] - blocks[i,j]) * spread_rate
+                delta_blocks[i,j] += (blocks[i,j-1] - blocks[i,j]) * spread_rates[i,j] * spread_rates[i,j-1]
             
             if i == height-1:
                 delta_blocks[i,j] += 0
             else :
-                delta_blocks[i,j] += (blocks[i+1,j] - blocks[i,j]) * spread_rate
+                delta_blocks[i,j] += (blocks[i+1,j] - blocks[i,j]) * spread_rates[i,j] * spread_rates[i+1,j]
             
             if j == width-1:
                 delta_blocks[i,j] += 0
             else :
-                delta_blocks[i,j] += (blocks[i,j+1] - blocks[i,j]) * spread_rate
+                delta_blocks[i,j] += (blocks[i,j+1] - blocks[i,j]) * spread_rates[i,j] * spread_rates[i,j+1]
             
     blocks += delta_blocks
     delta_blocks = np.zeros((height, width))
     im = plt.imshow(blocks,cmap=plt.cm.jet,vmax=max,vmin=-min)
     cb = plt.colorbar()
     cb.set_ticks(np.linspace(min, max, 5)) 
-    plt.savefig(f"images//{k}.png")
+    plt.savefig(f"{k}.png")
     cb.remove()
     plt.cla()
     #time.sleep(1)
 
 for k in range(cycle):
-    im = Image.open(f"images/{k}.png")
+    im = Image.open(f"{k}.png")
+    images.append(im)
+    os.remove(f"{k}.png")
 
 images[0].save('images.gif',
                save_all=True, append_images=images[1:], optimize=False, duration=40, loop=0)

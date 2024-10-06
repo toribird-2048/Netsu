131dd007c18493f6eb83fa090b64fe5b742dcf1c
toribird-2048 <fly-toribird@outlook.jp>
2024-10-06 17:05:28 +0900

    cycle

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ height, width = 10,10
 blocks = np.zeros((height, width))
 delta_blocks = np.zeros((height, width))
 spread_rates = np.full((height, width),0.3)
-max = 200
+max = 100
 min = 0
-cycle = 300
+cycle = 10000
 
 fig = plt.figure()
 images = []
@@ -28,6 +28,7 @@ for k in range(height-2):
 
 for k in range(cycle):
     print(blocks)
+    print(k)
     blocks[0,0] = 100
     for i in range(height):
         for j in range(width):
@@ -56,15 +57,14 @@ for k in range(cycle):
     im = plt.imshow(blocks,cmap=plt.cm.jet,vmax=max,vmin=-min)
     cb = plt.colorbar()
     cb.set_ticks(np.linspace(min, max, 5)) 
-    plt.savefig(f"{k}.png")
+    plt.savefig(f"images/{k}.png")
     cb.remove()
     plt.cla()
     #time.sleep(1)
 
 for k in range(cycle):
-    im = Image.open(f"{k}.png")
-    images.append(im)
-    os.remove(f"{k}.png")
+    images.append(Image.open(f"images/{k}.png"))
+    os.remove(f"images{k}.png")
 
 images[0].save('images.gif',
                save_all=True, append_images=images[1:], optimize=False, duration=40, loop=0)
